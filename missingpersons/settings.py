3ebae4205af0e22414e64845a398f6d3f7faf38c
jnewell-7 <jacksonnewell07@gmail.com>
2022-11-14 16:03:57 -0700

    change databases in settings

--- missingpersons/settings.py
+++ missingpersons/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'missingpersons.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'missing.persons',
+        'USER': 'postgres',
+        'PASSWORD': 'Slayer4days!',
+        'HOST': 'localhost'
     }
 }
 
