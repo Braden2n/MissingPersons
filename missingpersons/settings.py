126fe62ecb6fc267276729dd1af89a75bac0e255
jnewell-7 <jacksonnewell07@gmail.com>
2022-11-16 12:31:20 -0700

    password change

--- missingpersons/settings.py
+++ missingpersons/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'missing.persons',
         'USER': 'postgres',
-        'PASSWORD': 'Slayer4days!',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost'
     }
 }
