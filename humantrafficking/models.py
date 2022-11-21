3952b8603f0ed9ca960b4502fc99140f7433361c
Thomas Cassler <twfcassler@gmail.com>
2022-11-21 16:15:40 -0700

    Update models.py

--- humantrafficking/models.py
+++ humantrafficking/models.py
@@ -4,5 +4,16 @@ from django.db import models
 
 
 class Person(models.Model):
+    date_missing = models.DateField()
+    last_name = models.CharField(max_length=45)
+    first_name = models.CharField(max_length=45)
+    age_at_missing = models.IntegerField()
+    city = models.CharField(max_length=45)
+    state = models.CharField(max_length=2)
+    gender = models.CharField(max_length=1)
+    race = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.last_name
     class Meta():
         db_table = "person"
