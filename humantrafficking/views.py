84e53aef8576fec6b749d904c841f1e551a95e9c
Thomas Cassler <twfcassler@gmail.com>
2022-11-21 17:36:50 -0700

    Changes to display models input to homepage.

--- humantrafficking/views.py
+++ humantrafficking/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from .models import Person
 
 # Create your views here.
 
 
 def indexPageView(request):
-    return render(request, 'humantrafficking/index.html')
+    db_persons = Person.objects.all()
+
+    context = {
+        "persons": db_persons
+    }
+
+    return render(request, 'humantrafficking/index.html', context)
 
 
 def informationPageView(request):
