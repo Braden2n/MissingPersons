e052624fc807b83524b2267505fe65d1b1e01aae
e-b11 <96320276+e-b11@users.noreply.github.com>
2022-12-07 17:55:05 -0700

    addpageupdates

--- humantrafficking/views.py
+++ humantrafficking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Person
 
 # Create your views here.
@@ -34,4 +34,34 @@ def searchPageView(request):
 
 
 def addPageView(request):
-    return render(request, 'humantrafficking/add.html')
+    if request.method == "POST":
+       date_missing = request.POST["date_at_missing"]
+       last_name = request.POST["last_name"]
+       first_name = request.POST["first_name"]
+       age_at_missing = request.POST["age_at_missing"]
+       city = request.POST["city"]
+       state = request.POST["state"]
+       gender = request.POST["gender"]
+       race = request.POST["race"]
+
+       new_person = Person()
+
+       new_person.date_missing = date_missing
+       new_person.last_name = last_name
+       new_person.first_name = first_name
+       new_person.age_at_missing = age_at_missing
+       new_person.city = city
+       new_person.state = state
+       new_person.gender = gender
+       new_person.race = race
+
+       new_person.save()
+
+       return redirect("index")
+    else:
+        persons = Person.objects.all()
+
+    context = {
+        "persons": persons
+    }
+    return render(request, 'humantrafficking/add.html', context)
